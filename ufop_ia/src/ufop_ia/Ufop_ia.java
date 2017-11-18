8e00e1886e0b5271fac1eecc3eb4c6fe64ac45d8
Arthur Miranda Gomes <arthur-m-gomes@hotmail.com>
2017-11-18 14:34:54 -0200

    novo

---
+++ ufop_ia/src/ufop_ia/Ufop_ia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufop_ia;
+
+/**
+ *
+ * @author arthu
+ */
+public class Ufop_ia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
