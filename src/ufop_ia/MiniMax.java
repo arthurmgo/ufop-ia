bfb675d06bc5bdf535e1ad7513daf3404be5a698
Arthur Miranda Gomes <arthur-m-gomes@hotmail.com>
2017-11-19 14:07:39 -0200

    update

--- src/ufop_ia/MiniMax.java
+++ src/ufop_ia/MiniMax.java
@@ -10,12 +10,46 @@ package ufop_ia;
  * @author arthu
  */
 public class MiniMax {
-    public static void main(String[] args) {
-      
-        System.out.print("ok");
-        
-        
+
+    public EstadoJogo miniMaxDecision(EstadoJogo st) {
+        return null;
+
     }
     
+    private int nextPlayer(int p){
+        if(p == 1){
+            return 0;
+        }
+        return 1;
+    }
+
+    public Double maxValue(EstadoJogo st, int player) throws CloneNotSupportedException {
+
+        if (st.isTerminal()) {
+            return st.getValue().doubleValue();
+        }
+        
+        Double v = Double.NEGATIVE_INFINITY;
+        for(Tabuleiro filhos : st.estadosAlcancaveis(player)){
+            v = Math.max(v, minValue(new EstadoJogo(filhos), nextPlayer(player)));
+        }
+        return v;
+
+    }
+    
+    
+    public Double minValue(EstadoJogo st, int player) throws CloneNotSupportedException {
+
+        if (st.isTerminal()) {
+            return st.getValue().doubleValue();
+        }
+        
+        Double v = Double.NEGATIVE_INFINITY;
+        for(Tabuleiro filhos : st.estadosAlcancaveis(player)){
+            v = Math.min(v, maxValue(new EstadoJogo(filhos), nextPlayer(player)));
+        }
+        return v;
+
+    }
     
 }
