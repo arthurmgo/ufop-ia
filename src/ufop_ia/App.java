65894f9aff44694106ac15d717fad2dd3558d222
jean <jeankennedteodoro@gmail.com>
2017-11-21 20:44:03 -0200

    Release2.0

---
+++ src/ufop_ia/App.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ufop_ia;
+
+/**
+ *
+ * @author arthu
+ */
+public class App {
+    public static void main(String[] args) {
+        Tabuleiro t1 = new Tabuleiro(4, 4);
+       // t1.iniciaTabuleiro4();
+        int tab[][]={{0,0,0,0},{0,0,0,0},{0,0,0,-1},{1,0,2,0}};
+        t1.setTab(tab);
+        EstadoJogo inicial = new EstadoJogo(t1);
+        
+        //System.out.println(inicial.getTab());
+        
+        //System.out.println(inicial.estadosAlcancaveis(0));
+        
+//        for (EstadoJogo a : inicial.estadosAlcancaveis(0)) {
+//            System.out.println(a.estadosAlcancaveis(0));
+//        }
+        
+
+        MiniMax m1 = new MiniMax();
+        
+        System.out.println("estado:\n"+m1.miniMaxDecision(inicial));
+        
+        
+        
+        //System.out.println(m1.miniMaxDecision(inicial, 1).getTab());
+    }
+    
+}
