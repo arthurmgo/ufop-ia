ffb959abb9e5e14558486032daafe4289f83d44b
jean <jeankennedteodoro@gmail.com>
2017-11-21 20:44:10 -0200

    Release2.0

--- src/ufop_ia/MiniMax.java
+++ src/ufop_ia/MiniMax.java
@@ -12,44 +12,74 @@ package ufop_ia;
 public class MiniMax {
 
     public EstadoJogo miniMaxDecision(EstadoJogo st) {
+        
+        Double v = maxValue(st);
+        
+        for (EstadoJogo filho : st.estadosAlcancaveis(1)) {
+            
+            System.out.println(filho.getTab());
+
+            if (filho.getValue() == v) {
+                return filho;
+            }
+        }
         return null;
 
     }
-    
-    private int nextPlayer(int p){
-        if(p == 1){
-            return 0;
-        }
-        return 1;
-    }
 
-    public Double maxValue(EstadoJogo st, int player) throws CloneNotSupportedException {
+//    private int nextPlayer(int p) {
+//        if (p == 1) {
+//            System.out.println("o jogador é o lobo");
+//            return 0;
+//        }
+//        System.out.println("o jogador é a ovelha");
+//        return 1;
+//    }
+
+    public Double maxValue(EstadoJogo st) {
+
+        int player = 1; // A ovelha é o MAX
 
         if (st.isTerminal()) {
-            return st.getValue().doubleValue();
+         //   System.out.println("Terminal1!\n\n\n\n\n\n\n\n\n\n\n");
+            System.out.println(st);
+            System.out.println(st.getValue());
+            return st.getValue();
         }
-        
+
         Double v = Double.NEGATIVE_INFINITY;
-        for(Tabuleiro filhos : st.estadosAlcancaveis(player)){
-            v = Math.max(v, minValue(new EstadoJogo(filhos), nextPlayer(player)));
+
+        for (EstadoJogo filho : st.estadosAlcancaveis(player)) {
+            System.out.println(filho.getTab());
+            v = Math.max(v, minValue(filho));
+            filho.setValue(v);
+
         }
         return v;
 
     }
-    
-    
-    public Double minValue(EstadoJogo st, int player) throws CloneNotSupportedException {
+
+    public Double minValue(EstadoJogo st) {
+
+        int player = 0; // O lobo é o MIN
 
         if (st.isTerminal()) {
-            return st.getValue().doubleValue();
+          System.out.println("Terminal2:::!\n\n\n\n\n\n\n\n\n\n\n");
+           System.out.println(st);
+           System.out.println(st.getValue());
+            return st.getValue();
         }
-        
+
         Double v = Double.POSITIVE_INFINITY;
-        for(Tabuleiro filhos : st.estadosAlcancaveis(player)){
-            v = Math.min(v, maxValue(new EstadoJogo(filhos), nextPlayer(player)));
+        
+        for (EstadoJogo filho : st.estadosAlcancaveis(player)) {
+            System.out.println(filho.getTab());
+            v = Math.min(v, maxValue(filho));
+            filho.setValue(v);
         }
         return v;
 
     }
-    
+
 }
+    
