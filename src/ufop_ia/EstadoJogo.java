479c274ca0610094f155f64c544055a1410fa689
jean <jeankennedteodoro@gmail.com>
2017-11-21 20:43:53 -0200

    Release2.0

--- src/ufop_ia/EstadoJogo.java
+++ src/ufop_ia/EstadoJogo.java
@@ -14,15 +14,36 @@ import java.util.ArrayList;
 public class EstadoJogo {
 
     private Tabuleiro tab;
+    
+    // Estou tentando criar uma arvore
+    private ArrayList<EstadoJogo> filhos;
+    
     private double value;
 
-    public EstadoJogo(Tabuleiro tab) throws CloneNotSupportedException {
+    public EstadoJogo(Tabuleiro tab) {
         this.tab = tab.clone();
+
+    }
+    
+    // Aqui eu retorno os filhos dessa arvore ou crio ela, devo fazer isso aqui?
+    public ArrayList<EstadoJogo> estadosAlcancaveis(int idVez) {
+        if (this.filhos == null) {
+            this.filhos = new ArrayList<>();
+            ArrayList<Tabuleiro> tabuleiros = new  ArrayList<Tabuleiro>();
+            tabuleiros.addAll(tabuleirosAlcancaveis(idVez));
+            if(tabuleiros.isEmpty()){
+                return this.filhos;
+            }
+            for (Tabuleiro novo : tabuleiros) {
+                this.filhos.add(new EstadoJogo(novo));
+            }
+        }
+        return this.filhos;
     }
 
     //idVez indica a vez de jogar
     //0 para lobo, 1 para ovelhas
-    public ArrayList<Tabuleiro> estadosAlcancaveis(int idVez) throws CloneNotSupportedException {
+    public ArrayList<Tabuleiro> tabuleirosAlcancaveis(int idVez) {
         ArrayList<Tabuleiro> alcancaveis = new ArrayList<Tabuleiro>();
         if (idVez == 0) {
             int posicaoLobo[];
@@ -34,9 +55,9 @@ public class EstadoJogo {
             //Primeira Diagonal
             line--;
             column++;
-            if (line >= 0 && line <= 7 && column >= 0 && column <= 7) {
+            if (line >= 0 && line < tab.getTab().length && column >= 0 && column < tab.getTab()[0].length) {
                 if (tab.getTab()[line][column] == 0) {
-                    int tabNovo[][] = new int[8][8];
+                    int tabNovo[][] = new int[tab.getTab().length][tab.getTab()[0].length];
                     for (int i = 0; i < tab.getTab().length; i++) {
                         for (int j = 0; j < tab.getTab()[0].length; j++) {
                             tabNovo[i][j] = tab.getTab()[i][j];
@@ -58,9 +79,9 @@ public class EstadoJogo {
 
             line--;
             column--;
-            if (line >= 0 && line <= 7 && column >= 0 && column <= 7) {
+            if (line >= 0 && line < tab.getTab().length && column >= 0 && column < tab.getTab()[0].length) {
                 if (tab.getTab()[line][column] == 0) {
-                    int tabNovo[][] = new int[8][8];
+                   int tabNovo[][] = new int[tab.getTab().length][tab.getTab()[0].length];
                     for (int i = 0; i < tab.getTab().length; i++) {
                         for (int j = 0; j < tab.getTab()[0].length; j++) {
                             tabNovo[i][j] = tab.getTab()[i][j];
@@ -81,9 +102,9 @@ public class EstadoJogo {
 
             line++;
             column++;
-            if (line >= 0 && line <= 7 && column >= 0 && column <= 7) {
+            if (line >= 0 && line < tab.getTab().length && column >= 0 && column < tab.getTab()[0].length) {
                 if (tab.getTab()[line][column] == 0) {
-                    int tabNovo[][] = new int[8][8];
+                   int tabNovo[][] = new int[tab.getTab().length][tab.getTab()[0].length];
                     for (int i = 0; i < tab.getTab().length; i++) {
                         for (int j = 0; j < tab.getTab()[0].length; j++) {
                             tabNovo[i][j] = tab.getTab()[i][j];
@@ -103,9 +124,9 @@ public class EstadoJogo {
 
             line++;
             column--;
-            if (line >= 0 && line <= 7 && column >= 0 && column <= 7) {
+            if (line >= 0 && line < tab.getTab().length && column >= 0 && column < tab.getTab()[0].length) {
                 if (tab.getTab()[line][column] == 0) {
-                    int tabNovo[][] = new int[8][8];
+                    int tabNovo[][] = new int[tab.getTab().length][tab.getTab()[0].length];
                     for (int i = 0; i < tab.getTab().length; i++) {
                         for (int j = 0; j < tab.getTab()[0].length; j++) {
                             tabNovo[i][j] = tab.getTab()[i][j];
@@ -134,9 +155,9 @@ public class EstadoJogo {
             //1 diagonal
             line--;
             column--;
-            if (line >= 0 && line <= 7 && column >= 0 && column <= 7) {
+            if (line >= 0 && line < tab.getTab().length && column >= 0 && column < tab.getTab()[0].length) {
                 if (tab.getTab()[line][column] == 0) {
-                    int tabNovo[][] = new int[8][8];
+                    int tabNovo[][] = new int[tab.getTab().length][tab.getTab()[0].length];
                     for (int i = 0; i < tab.getTab().length; i++) {
                         for (int j = 0; j < tab.getTab()[0].length; j++) {
                             tabNovo[i][j] = tab.getTab()[i][j];
@@ -156,9 +177,9 @@ public class EstadoJogo {
             column = posicaoOvelha1[1];
             line--;
             column++;
-            if (line >= 0 && line <= 7 && column >= 0 && column <= 7) {
+            if (line >= 0 && line < tab.getTab().length && column >= 0 && column < tab.getTab()[0].length) {
                 if (tab.getTab()[line][column] == 0) {
-                    int tabNovo[][] = new int[8][8];
+                   int tabNovo[][] = new int[tab.getTab().length][tab.getTab()[0].length];
                     for (int i = 0; i < tab.getTab().length; i++) {
                         for (int j = 0; j < tab.getTab()[0].length; j++) {
                             tabNovo[i][j] = tab.getTab()[i][j];
@@ -183,9 +204,9 @@ public class EstadoJogo {
             //1 diagonal
             line--;
             column--;
-            if (line >= 0 && line <= 7 && column >= 0 && column <= 7) {
+            if (line >= 0 && line < tab.getTab().length && column >= 0 && column < tab.getTab()[0].length) {
                 if (tab.getTab()[line][column] == 0) {
-                    int tabNovo[][] = new int[8][8];
+                  int tabNovo[][] = new int[tab.getTab().length][tab.getTab()[0].length];
                     for (int i = 0; i < tab.getTab().length; i++) {
                         for (int j = 0; j < tab.getTab()[0].length; j++) {
                             tabNovo[i][j] = tab.getTab()[i][j];
@@ -205,9 +226,9 @@ public class EstadoJogo {
             column = posicaoOvelha2[1];
             line--;
             column++;
-            if (line >= 0 && line <= 7 && column >= 0 && column <= 7) {
+            if (line >= 0 && line < tab.getTab().length && column >= 0 && column < tab.getTab()[0].length) {
                 if (tab.getTab()[line][column] == 0) {
-                    int tabNovo[][] = new int[8][8];
+                    int tabNovo[][] = new int[tab.getTab().length][tab.getTab()[0].length];
                     for (int i = 0; i < tab.getTab().length; i++) {
                         for (int j = 0; j < tab.getTab()[0].length; j++) {
                             tabNovo[i][j] = tab.getTab()[i][j];
@@ -232,9 +253,9 @@ public class EstadoJogo {
             //1 diagonal
             line--;
             column--;
-            if (line >= 0 && line <= 7 && column >= 0 && column <= 7) {
+            if (line >= 0 && line < tab.getTab().length && column >= 0 && column < tab.getTab()[0].length) {
                 if (tab.getTab()[line][column] == 0) {
-                    int tabNovo[][] = new int[8][8];
+                    int tabNovo[][] = new int[tab.getTab().length][tab.getTab()[0].length];
                     for (int i = 0; i < tab.getTab().length; i++) {
                         for (int j = 0; j < tab.getTab()[0].length; j++) {
                             tabNovo[i][j] = tab.getTab()[i][j];
@@ -254,9 +275,9 @@ public class EstadoJogo {
             column = posicaoOvelha3[1];
             line--;
             column++;
-            if (line >= 0 && line <= 7 && column >= 0 && column <= 7) {
+            if (line >= 0 && line < tab.getTab().length && column >= 0 && column < tab.getTab()[0].length) {
                 if (tab.getTab()[line][column] == 0) {
-                    int tabNovo[][] = new int[8][8];
+                    int tabNovo[][] = new int[tab.getTab().length][tab.getTab()[0].length];
                     for (int i = 0; i < tab.getTab().length; i++) {
                         for (int j = 0; j < tab.getTab()[0].length; j++) {
                             tabNovo[i][j] = tab.getTab()[i][j];
@@ -281,9 +302,9 @@ public class EstadoJogo {
             //1 diagonal
             line--;
             column--;
-            if (line >= 0 && line <= 7 && column >= 0 && column <= 7) {
+            if (line >= 0 && line < tab.getTab().length && column >= 0 && column < tab.getTab()[0].length) {
                 if (tab.getTab()[line][column] == 0) {
-                    int tabNovo[][] = new int[8][8];
+                    int tabNovo[][] = new int[tab.getTab().length][tab.getTab()[0].length];
                     for (int i = 0; i < tab.getTab().length; i++) {
                         for (int j = 0; j < tab.getTab()[0].length; j++) {
                             tabNovo[i][j] = tab.getTab()[i][j];
@@ -303,9 +324,9 @@ public class EstadoJogo {
             column = posicaoOvelha4[1];
             line--;
             column++;
-            if (line >= 0 && line <= 7 && column >= 0 && column <= 7) {
+            if (line >= 0 && line < tab.getTab().length && column >= 0 && column < tab.getTab()[0].length) {
                 if (tab.getTab()[line][column] == 0) {
-                    int tabNovo[][] = new int[8][8];
+                    int tabNovo[][] = new int[tab.getTab().length][tab.getTab()[0].length];
                     for (int i = 0; i < tab.getTab().length; i++) {
                         for (int j = 0; j < tab.getTab()[0].length; j++) {
                             tabNovo[i][j] = tab.getTab()[i][j];
@@ -325,46 +346,56 @@ public class EstadoJogo {
         return alcancaveis;
     }
 
-    boolean isTerminal() throws CloneNotSupportedException {
+    boolean isTerminal() {
 
         return loboLose() || loboWin();
 
     }
 
-    public double getValue() throws CloneNotSupportedException {
-        if(loboWin()){
-            return 1;
-        }else if(loboLose()){
+    public double getValue() {
+        if (loboWin()) {
             return -1;
-        }else{
+        } else if (loboLose()) {
+            return 1;
+        } else {
             return this.value;
         }
 
+    }
+
+    public Tabuleiro getTab() {
+        return tab;
+    }
+
+    public void setTab(Tabuleiro tab) {
+        this.tab = tab;
     }
 
     public void setValue(double value) {
         this.value = value;
     }
 
-    public boolean loboWin() throws CloneNotSupportedException {
+    public boolean loboWin() {
         int posicaoLobo[] = new int[2];
         posicaoLobo = tab.buscaPosicao(-1);
         if (posicaoLobo[0] == 7) {
             return true;
         }
-        if (estadosAlcancaveis(1).size() == 0 && estadosAlcancaveis(1).size() != 0) {
-            return true;
-        }
-        return false;
+        // return estadosAlcancaveis(1).isEmpty() && !estadosAlcancaveis(0).isEmpty();
+        
+        return this.tabuleirosAlcancaveis(1).isEmpty() && !this.tabuleirosAlcancaveis(0).isEmpty();
 
     }
 
-    public boolean loboLose() throws CloneNotSupportedException {
-        if (!(loboWin()) && estadosAlcancaveis(0).size() == 0) {
-            return true;
-
-        }
-        return false;
+    public boolean loboLose() {
+        return !loboWin() && this.tabuleirosAlcancaveis(0).isEmpty();
     }
+
+    @Override
+    public String toString() {
+        return this.tab.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
 
 }
